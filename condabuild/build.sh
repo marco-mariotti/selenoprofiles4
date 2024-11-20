fbf96f669ba5bc50e592f93a4e935c92e568188c
marco.mariotti <marco.mariotti.mm@gmail.com>
2024-11-20 16:38:38 +0100

    v4.5 on conda

---
+++ condabuild/build.sh
@@ -0,0 +1,1 @@
+mamba build -c mmariotti -c anaconda -c bioconda -c biobuilds .  2>&1 | tee build.log
---
+++ condabuild/install.sh
@@ -0,0 +1,1 @@
+mamba install -y -c mmariotti -c anaconda -c bioconda -c biobuilds selenoprofiles4 --use-local
